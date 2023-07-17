9494749fda659ec1ffa2519684129ef51a8dde99
Maxime Boucher <maxime@maximeboucher.com>
2023-07-17 11:06:46 -0600

    Work on submit form

--- src/componenents/SubmitForm.jsx
+++ src/componenents/SubmitForm.jsx
@@ -1,33 +1,30 @@
-import Card from "../UI/Card";
 import classes from "./SubmitForm.module.css";
 
 function SubmitForm() {
   return (
-    <Card>
-      <form className={classes.form}>
-        <div className={classes.control}>
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" required></input>
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="tagline">Tagline</label>
-          <input type="text" id="tagline" required></input>
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="link">Link</label>
-          <input type="url" id="link" required></input>
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="twitter">Twitter Account (optional)</label>
-          <input type="url" id="twitter" required={false}></input>
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="description">Description</label>
-          <textarea rows="3" id="description" required></textarea>
-        </div>
-        <button>Submit Tool</button>
-      </form>
-    </Card>
+    <form className={classes.form}>
+      <div className={classes.control}>
+        <label htmlFor="title">Title</label>
+        <input type="text" id="title" required></input>
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="tagline">Tagline</label>
+        <input type="text" id="tagline" required></input>
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="link">Link</label>
+        <input type="url" id="link" required></input>
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="twitter">Twitter Account (optional)</label>
+        <input type="url" id="twitter" required={false}></input>
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="description">Description</label>
+        <textarea rows="3" id="description" required></textarea>
+      </div>
+      <button>Submit Tool</button>
+    </form>
   );
 }
 

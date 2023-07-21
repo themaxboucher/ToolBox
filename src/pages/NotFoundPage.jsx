5837472e81b7da1014ec4a710544f0ca9f89a24a
Maxime Boucher <maxime@maximeboucher.com>
2023-07-20 19:06:36 -0600

    Fix className bug

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -4,7 +4,7 @@ function NotFoundPage() {
   return (
     <div>
       <h1>This Page Does Not Exist.</h1>
-      <Link to="/" class="btn">Go Home</Link>
+      <Link to="/" className="btn">Go Home</Link>
     </div>
   );
 }

851b5e028d63552c902b19b242881bc9327cf45b
Maxime Boucher <maxime@maximeboucher.com>
2023-07-30 11:26:02 -0600

    Add success section + other changes

--- src/componenents/SubmitForm.jsx
+++ src/componenents/SubmitForm.jsx
@@ -2,7 +2,12 @@ import Card from "../UI/Card";
 import classes from "./SubmitForm.module.css";
 import { useRef, useState } from "react";
 import { categoryOptions } from "../data/FilterOptions";
-import { CheckIcon, SparklesIcon } from "@heroicons/react/24/solid";
+import {
+  CheckCircleIcon,
+  CheckIcon,
+  SparklesIcon,
+} from "@heroicons/react/24/solid";
+import { Link } from "react-router-dom/cjs/react-router-dom";
 
 import { db } from "../Utilities/firebase";
 import { collection, addDoc, serverTimestamp } from "firebase/firestore";
@@ -10,6 +15,8 @@ import { auth } from "../Utilities/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 function SubmitForm() {
+  const [isSuccess, setIsSuccess] = useState(false);
+
   const [user] = useAuthState(auth);
 
   const nameInputRef = useRef();
@@ -70,7 +77,7 @@ function SubmitForm() {
       const docData = {
         public: false,
         createdAt: serverTimestamp(),
-        submitedBy: user.uid, 
+        submittedBy: user.uid,
         name: enteredName,
         link: enteredLink,
         tagline: enteredTagline,
@@ -86,6 +93,7 @@ function SubmitForm() {
         keyFeatures: enteredKeyFeatures,
       };
       await addDoc(collection(db, "tools"), docData);
+      setIsSuccess(true);
     } catch (error) {
       console.log(error);
     }
@@ -104,174 +112,221 @@ function SubmitForm() {
 
   return (
     <Card>
-      <div className={classes.heading}>
-        <h1><SparklesIcon /><span>Submit an AI tool</span></h1>
-        <p>
-          Know of a great AI tool we're missing? Built one yourself? Submit it
-          below.
-        </p>
-      </div>
-      <form className={classes.form} onSubmit={submitHandler}>
-        <div className={classes.row}>
-          <div className={classes.control}>
-            <label htmlFor="name">Tool Name</label>
-            <input
-              type="text"
-              id="name"
-              required
-              maxLength="35"
-              ref={nameInputRef}
-            ></input>
+      {!isSuccess ? (
+        <>
+          <div className={classes.heading}>
+            <h1>
+              <SparklesIcon />
+              <span>Submit an AI Tool</span>
+            </h1>
+            <p>
+              Know of a great AI tool we're missing? Built one yourself? Submit
+              it below.
+            </p>
           </div>
-          <div className={classes.control}>
-            <label htmlFor="link">Link to Tool</label>
-            <input type="url" id="link" required ref={linkInputRef}></input>
-          </div>
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="tagline">
-            <span>Tagline</span>
-            <span className={classes.sublabel}>
-              One sentece that describes what this AI tool does
-            </span>
-          </label>
-          <input
-            type="text"
-            id="tagline"
-            required
-            minLength="4"
-            maxLength="60"
-            ref={taglineInputRef}
-          />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="thumbnail">
-            <span>Cover Photo</span>
-            <span className={classes.sublabel}>Please provide a valid URL</span>
-          </label>
-          <input
-            type="url"
-            id="thumbnail"
-            required={false}
-            ref={thumbnailInputRef}
-          ></input>
-        </div>
-        <div className={classes.control}>
-          <label>
-            <span>Tool Categories</span>
-            <span className={classes.sublabel}>
-              Select at least one option:
-            </span>
-          </label>
-          <ul className={classes.tagList}>
-            {categoryOptions
-              .filter((category) => category !== "Featured") // Remove "Featured" from the array
-              .map((category) => (
-                <div
-                  key={category}
-                  className={
-                    !enteredTags.some((tag) => tag === category)
-                      ? classes.tag
-                      : classes.selectedTag
-                  }
-                  onClick={() => tagClickHandler(category)}
-                >
-                  {category}
-                </div>
-              ))}
-          </ul>
-        </div>
-        <div className={classes.control}>
-          <label>
-            <span>Pricing</span>
-            <span className={classes.sublabel}>
-              Select the pricing options this tool offers:
-            </span>
-          </label>
-          <div
-            className={!enteredFree ? classes.option : classes.selectedOption}
-            onClick={freeOptionClickHandler}
-          >
-            <span className={classes.checkbox}>
-              {enteredFree && <CheckIcon />}
-            </span>
-            <p>Free option</p>
-          </div>
-          <div
-            className={!enteredPaid ? classes.option : classes.selectedOption}
-            onClick={paidOptionClickHandler}
-          >
-            <span className={classes.checkbox}>
-              {enteredPaid && <CheckIcon />}
-            </span>
-            <p>Paid option</p>
-          </div>
-          {enteredPaid && (
+          <form className={classes.form} onSubmit={submitHandler}>
+            <div className={classes.row}>
+              <div className={classes.control}>
+                <label htmlFor="name">Tool Name</label>
+                <input
+                  type="text"
+                  id="name"
+                  required
+                  maxLength="35"
+                  ref={nameInputRef}
+                ></input>
+              </div>
+              <div className={classes.control}>
+                <label htmlFor="link">Link to Tool</label>
+                <input type="url" id="link" required ref={linkInputRef}></input>
+              </div>
+            </div>
             <div className={classes.control}>
-              <label htmlFor="price">
-                <span>Price</span>
+              <label htmlFor="tagline">
+                <span>Tagline</span>
                 <span className={classes.sublabel}>
-                  What is the cheapest paid option?
+                  One sentece that describes what this AI tool does
                 </span>
               </label>
               <input
                 type="text"
-                id="price"
-                required={enteredPaid ? true : false}
-                maxLength="40"
-                placeholder="e.g. From $10/mo"
-                ref={priceInputRef}
+                id="tagline"
+                required
+                minLength="4"
+                maxLength="60"
+                ref={taglineInputRef}
               />
             </div>
-          )}
+            <div className={classes.control}>
+              <label htmlFor="thumbnail">
+                <span>Cover Photo</span>
+                <span className={classes.sublabel}>
+                  Please provide a valid photo URL
+                </span>
+              </label>
+              <input
+                type="url"
+                id="thumbnail"
+                required={false}
+                ref={thumbnailInputRef}
+              ></input>
+            </div>
+            <div className={classes.control}>
+              <label>
+                <span>Tool Categories</span>
+                <span className={classes.sublabel}>
+                  Select at least one option:
+                </span>
+              </label>
+              <ul className={classes.tagList}>
+                {categoryOptions
+                  .filter((category) => category !== "Featured") // Remove "Featured" from the array
+                  .map((category) => (
+                    <div
+                      key={category}
+                      className={
+                        !enteredTags.some((tag) => tag === category)
+                          ? classes.tag
+                          : classes.selectedTag
+                      }
+                      onClick={() => tagClickHandler(category)}
+                    >
+                      {category}
+                    </div>
+                  ))}
+              </ul>
+            </div>
+            <div className={classes.control}>
+              <label>
+                <span>Pricing</span>
+                <span className={classes.sublabel}>
+                  Select the pricing options this tool offers:
+                </span>
+              </label>
+              <div
+                className={
+                  !enteredFree ? classes.option : classes.selectedOption
+                }
+                onClick={freeOptionClickHandler}
+              >
+                <span className={classes.checkbox}>
+                  {enteredFree && <CheckIcon />}
+                </span>
+                <p>Free option</p>
+              </div>
+              <div
+                className={
+                  !enteredPaid ? classes.option : classes.selectedOption
+                }
+                onClick={paidOptionClickHandler}
+              >
+                <span className={classes.checkbox}>
+                  {enteredPaid && <CheckIcon />}
+                </span>
+                <p>Paid option</p>
+              </div>
+              {enteredPaid && (
+                <div className={classes.control}>
+                  <label htmlFor="price">
+                    <span>Price</span>
+                    <span className={classes.sublabel}>
+                      What is the cheapest paid option?
+                    </span>
+                  </label>
+                  <input
+                    type="text"
+                    id="price"
+                    required={enteredPaid ? true : false}
+                    maxLength="50"
+                    placeholder="e.g. From $10/mo"
+                    ref={priceInputRef}
+                  />
+                </div>
+              )}
+            </div>
+            <div className={classes.control}>
+              <label>Key Features</label>
+              <label htmlFor="keyFeature1" className={classes.sublabel}>
+                #1
+              </label>
+              <textarea
+                rows="2"
+                id="keyFeature1"
+                required
+                minLength="10"
+                maxLength="350"
+                ref={keyFeature1InputRef}
+              ></textarea>
+              <label htmlFor="keyFeature2" className={classes.sublabel}>
+                #2
+              </label>
+              <textarea
+                rows="2"
+                id="keyFeature2"
+                required
+                minLength="10"
+                maxLength="350"
+                ref={keyFeature2InputRef}
+              ></textarea>
+              <label htmlFor="keyFeature3" className={classes.sublabel}>
+                #3 (optional)
+              </label>
+              <textarea
+                rows="2"
+                id="keyFeature3"
+                minLength="10"
+                maxLength="350"
+                ref={keyFeature3InputRef}
+              ></textarea>
+              <label htmlFor="keyFeature4" className={classes.sublabel}>
+                #4 (optional)
+              </label>
+              <textarea
+                rows="2"
+                id="keyFeature4"
+                minLength="10"
+                maxLength="350"
+                ref={keyFeature4InputRef}
+              ></textarea>
+              <label htmlFor="keyFeature5" className={classes.sublabel}>
+                #5 (optional)
+              </label>
+              <textarea
+                rows="2"
+                id="keyFeature5"
+                minLength="10"
+                maxLength="350"
+                ref={keyFeature5InputRef}
+              ></textarea>
+            </div>
+            <button className="btn">
+              <SparklesIcon />
+              <span>Submit Tool</span>
+            </button>
+          </form>
+        </>
+      ) : (
+        <div className={classes.success}>
+          <div className={classes.heading}>
+            <h1>
+              <CheckCircleIcon />
+              <span>Success</span>
+            </h1>
+            <p>
+              Your AI tool what submitted successfully and should appear on
+              ToolBox shortly.
+            </p>
+          </div>
+          <div className={classes.buttons}>
+            <button className="btn" onClick={() => setIsSuccess(false)}>
+              Submit Another Tool
+            </button>
+            <Link to="/" className="btn-alt">
+              Back To Home Page
+            </Link>
+          </div>
         </div>
-        <div className={classes.control}>
-          <label>Key Features</label>
-          <label htmlFor="keyFeature1" className={classes.sublabel}>
-            #1
-          </label>
-          <textarea
-            rows="2"
-            id="keyFeature1"
-            required
-            ref={keyFeature1InputRef}
-          ></textarea>
-          <label htmlFor="keyFeature2" className={classes.sublabel}>
-            #2
-          </label>
-          <textarea
-            rows="2"
-            id="keyFeature2"
-            required
-            ref={keyFeature2InputRef}
-          ></textarea>
-          <label htmlFor="keyFeature3" className={classes.sublabel}>
-            #3 (optional)
-          </label>
-          <textarea
-            rows="2"
-            id="keyFeature3"
-            ref={keyFeature3InputRef}
-          ></textarea>
-          <label htmlFor="keyFeature4" className={classes.sublabel}>
-            #4 (optional)
-          </label>
-          <textarea
-            rows="2"
-            id="keyFeature4"
-            ref={keyFeature4InputRef}
-          ></textarea>
-          <label htmlFor="keyFeature5" className={classes.sublabel}>
-            #5 (optional)
-          </label>
-          <textarea
-            rows="2"
-            id="keyFeature5"
-            ref={keyFeature5InputRef}
-          ></textarea>
-        </div>
-        <button className="btn"><SparklesIcon /><span>Submit Tool</span></button>
-      </form>
+      )}
     </Card>
   );
 }

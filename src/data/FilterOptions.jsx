4a22dee97f3d18eac9626855e1c5e12839dd45a5
Maxime Boucher <maxime@maximeboucher.com>
2023-08-03 14:11:06 -0600

    Sort categoryOptions + add "Website" option

--- src/data/FilterOptions.jsx
+++ src/data/FilterOptions.jsx
@@ -1,5 +1,5 @@
 export const priceOptions = ["All", "Free", "Paid"];
-export const categoryOptions = [
+export const categoryOptions = sortArrayWithFirstElement([
   "Featured",
   "Chat",
   "Writing",
@@ -8,10 +8,24 @@ export const categoryOptions = [
   "Audio",
   "Podcast",
   "Code",
+  "Website",
   "SMM",
   "SEO",
   "API",
-];
+]);
+
+// Sort categoryOptions by alphabetical order but keep "Featured" as the first option
+function sortArrayWithFirstElement(arr) {
+  const firstElement = arr[0];
+  const restOfArray = arr.slice(1);
+
+  restOfArray.sort(); // Sort the rest of the array
+
+  // Insert the first element back at the beginning of the sorted array
+  restOfArray.unshift(firstElement);
+
+  return restOfArray;
+}
 
 // filterArray is an array of arrays that together contain objects with all the different filter combinations
 const filterArray = priceOptions.map((priceOption) => {
